ebb6be0026cbaa3a6b5afea8442f94544923f11f
christian <christianlouismingo@yahoo.com>
2021-07-27 18:22:25 +0800

    CM-remove log messages

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -3,6 +3,3 @@ import 'firebase/firestore'
 import firebaseConfig from './firebaseConfig'
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 export default firebaseApp.firestore();
-
-console.log(firebaseConfig);
-console.log('test');
