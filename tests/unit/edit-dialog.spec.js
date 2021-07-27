bb79489088f9e0a9a5df4c23763b84c3cd272359
christian <christianlouismingo@yahoo.com>
2021-07-27 18:03:06 +0800

    CM-add cancel and edit button test cases

--- tests/unit/edit-dialog.spec.js
+++ tests/unit/edit-dialog.spec.js
@@ -19,5 +19,26 @@ describe('Edit.vue', async () => {
     expect(wrapper.isVisible()).toBe(true);
   })
 
+  it('clicking cancel button should not perform a save', async () => {
+    const cancelButton = wrapper.find('.edit-cancel-button');
+    await cancelButton.trigger('click');
+    expect(wrapper.classes('.edit-dialog-snackbar')).toBe(false);
+  })
+
+  it('clicking edit button should perform a save', async () => {
+    const submitButton = wrapper.find('.edit-submit-button');
+
+    await wrapper.setData({
+      formData: {
+        type: "values",
+        title: "Sample test title",
+        description: "Sample test description"
+      }
+    });
+
+    await submitButton.trigger('click');
+    expect(wrapper.emitted()).toHaveProperty('close');
+  })
+
   
 })
